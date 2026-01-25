6f046ab6b63ef3c16ab646ea10b077b8d8470ea0
Aleksandr Kovalko <gistrec@gmail.com>
2026-01-25 15:57:52 +0700

    Add cached token handling with retry (#2)

--- auth/xbox.py
+++ auth/xbox.py
@@ -1,9 +1,59 @@
+import time
+from datetime import datetime, timezone
+
 import requests
+
+from db import get_setting, set_setting
 
 
 class XboxAuth:
     XBL_AUTH_URL = "https://user.auth.xboxlive.com/user/authenticate"
     XSTS_AUTH_URL = "https://xsts.auth.xboxlive.com/xsts/authorize"
+
+    XBL_TOKEN_KEY = "xbl_token"
+    XBL_EXPIRES_KEY = "xbl_token_expires"
+    XBL_UHS_KEY = "xbl_user_hash"
+
+    XSTS_TOKEN_KEY = "xsts_token"
+    XSTS_EXPIRES_KEY = "xsts_token_expires"
+    XSTS_UHS_KEY = "xsts_user_hash"
+
+    @staticmethod
+    def _parse_not_after(data: dict, fallback_seconds: int = 23 * 3600) -> int:
+        not_after = data.get("NotAfter")
+        if not_after:
+            try:
+                parsed = datetime.fromisoformat(not_after.replace("Z", "+00:00"))
+                return int(parsed.timestamp()) - 60
+            except ValueError:
+                pass
+        return int(time.time()) + fallback_seconds
+
+    def _store_xbl(self, data: dict) -> tuple[str, str]:
+        token = data["Token"]
+        uhs = data["DisplayClaims"]["xui"][0]["uhs"]
+        set_setting(self.XBL_TOKEN_KEY, token)
+        set_setting(self.XBL_UHS_KEY, uhs)
+        set_setting(self.XBL_EXPIRES_KEY, str(self._parse_not_after(data)))
+        return token, uhs
+
+    def _store_xsts(self, data: dict) -> tuple[str, str]:
+        token = data["Token"]
+        uhs = data["DisplayClaims"]["xui"][0]["uhs"]
+        set_setting(self.XSTS_TOKEN_KEY, token)
+        set_setting(self.XSTS_UHS_KEY, uhs)
+        set_setting(self.XSTS_EXPIRES_KEY, str(self._parse_not_after(data)))
+        return token, uhs
+
+    def get_xbl_token(self, ms_access_token: str) -> tuple[str, str]:
+        token = get_setting(self.XBL_TOKEN_KEY)
+        uhs = get_setting(self.XBL_UHS_KEY)
+        expires = get_setting(self.XBL_EXPIRES_KEY)
+
+        if token and uhs and expires and time.time() < float(expires):
+            return token, uhs
+
+        return self.authenticate(ms_access_token)
 
     def authenticate(self, ms_access_token: str) -> tuple[str, str]:
         """
@@ -29,10 +79,17 @@ class XboxAuth:
         r.raise_for_status()
         data = r.json()
 
-        return (
-            data["Token"],
-            data["DisplayClaims"]["xui"][0]["uhs"],
-        )
+        return self._store_xbl(data)
+
+    def get_xsts_token(self, xbl_token: str) -> tuple[str, str]:
+        token = get_setting(self.XSTS_TOKEN_KEY)
+        uhs = get_setting(self.XSTS_UHS_KEY)
+        expires = get_setting(self.XSTS_EXPIRES_KEY)
+
+        if token and uhs and expires and time.time() < float(expires):
+            return token, uhs
+
+        return self.authorize_xsts(xbl_token)
 
     def authorize_xsts(self, xbl_token: str) -> tuple[str, str]:
         """
@@ -57,7 +114,4 @@ class XboxAuth:
         r.raise_for_status()
         data = r.json()
 
-        return (
-            data["Token"],
-            data["DisplayClaims"]["xui"][0]["uhs"],
-        )
+        return self._store_xsts(data)
